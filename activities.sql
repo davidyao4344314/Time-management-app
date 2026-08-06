247b2cd7802fcfc3a91bd5227730871d512f92eb
david <davidyao996@gmail.com>
2026-08-06 17:14:07 +1200

    added adding stuff to the data base

--- activities.sql
+++ activities.sql
@@ -7,3 +7,12 @@ CREATE TABLE IF NOT EXISTS activities(
     start_time TEXT NOT NULL,
     end_time TEXT NOT NULL
 );
+CREATE TABLE IF NOT EXISTS exams(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    category TEXT NOT NULL,
+    SUJECT TEXT,
+    date TEXT NOT NULL,
+    start_time TEXT not null,
+    end_time TEXT NOT NULL
+)
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,38 @@
 import sqlite3
 from pathlib import Path
 
+
+def get_user_values(columns):
+    values = []
+
+    for column in columns:
+        user_value = input(f"Enter {column}: ").strip()
+
+        if user_value.lower() == "quit":
+            return None
+
+        values.append(user_value)
+
+    return values
+
+def get_clomuns(connection, table_name):
+    cursor = connection.execute(f"PRAGMA table_info({table_name})")
+    table_data = cursor.fetchall()
+
+    columns = []
+    for column in table_data:
+        comumn_name = column[1]
+
+        if comumn_name != "id":
+            columns.append(comumn_name)
+
+    return columns
+
+
+
 project_dir = Path(__file__).resolve().parents[2]
 backend_dir = Path(__file__).resolve().parent.parent
+
 sql_file = project_dir / "activities.sql"
 db_file = backend_dir / "study_app.db"
 
@@ -13,7 +43,36 @@ with open(sql_file, "r") as file:
 
 connection.executescript(sql_code)
 
-connection.commit()
-connection.close()
+columns = [
+    "name",
+    "category",
+    "subject",
+    "date",
+    "start_time",
+    "end_time"
+]
 
-print("Database and activities table created.")
+while True:
+    columns = get_clomuns(connection, "activities")
+    values = get_user_values(columns)
+
+    if values is None:
+        break
+
+    connection.execute("""
+        INSERT INTO activities (
+            name,
+            category,
+            subject,
+            date,
+            start_time,
+            end_time
+        )
+        VALUES (?, ?, ?, ?, ?, ?)
+    """, values)
+
+    connection.commit()
+
+    print("Activity added successfully.\n")
+
+connection.close()
